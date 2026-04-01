f8e6453ac05c0d367235a72eeae3df621bf95297
codeX4r <pandeyparth406@gmail.com>
2026-04-02 02:33:23 +0530

    deploy

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,16 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth} from "firebase/auth";
+import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD6xmtAivoYw121r2tJH_dGE3syLpESSng",
-  authDomain: "netflixgpt-d175c.firebaseapp.com",
-  projectId: "netflixgpt-d175c",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: "netflixgpt-d175c.firebasestorage.app",
   messagingSenderId: "662916129476",
   appId: "1:662916129476:web:d0157c3f33056138c69f91",
